5e4eca9eb0326873e78af54b97ab047d1529fe74
Khaled Ismail <khaled.ismail@amplitude.com>
2026-06-30 18:36:52 +0400

    Update battleboat.js

--- js/battleboat.js
+++ js/battleboat.js
@@ -8,7 +8,7 @@
   //await client.initializeAsync();
 	
 	async function initStatsig() {
-	  const client = new StatsigClient('client-s135N7Q1zaJeVD5MW4qpQA49UhobS02HvtPx7zVIX47', {
+	  const client = new StatsigClient("client-s135N7Q1zaJeVD5MW4qpQA49UhobS02HvtPx7zVIX47", {
 	    userID: "user-123"  // required
 	  });
 	  await client.initializeAsync();  // wait before evaluating
